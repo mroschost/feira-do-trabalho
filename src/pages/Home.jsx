91f7160d53f85db2a26022e26eff73e9481f3a4c
Henrikecrz <139394577+henrkecrz@users.noreply.github.com>
2026-06-26 15:46:55 -0300

    Atualiza SEO da Feira da Torre

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,20 +10,20 @@ const Home = () => {
   return (
     <>
       <Helmet>
-        <title>Feira do Trabalho e do Campo - DF</title>
+        <title>Última edição - Feira da Torre | Feira do Trabalho e do Campo DF</title>
         <meta 
           name="description" 
-          content="Feira do Trabalho e do Campo - DF, de 12 a 17 de maio de 2026, em Água Quente-DF." 
+          content="Última edição - Feira da Torre, de 30 de junho a 05 de julho de 2026, em Brasília-DF." 
         />
-        <meta property="og:title" content="Feira do Trabalho e do Campo - DF" />
+        <meta property="og:title" content="Última edição - Feira da Torre | Feira do Trabalho e do Campo DF" />
         <meta 
           property="og:description" 
-          content="De 12 a 17 de maio de 2026, em Água Quente-DF." 
+          content="De 30 de junho a 05 de julho de 2026, na Feira da Torre, em Brasília-DF." 
         />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://feiradotrabalhoedocampodf.com/" />
         <meta property="og:locale" content="pt_BR" />
-        <meta name="keywords" content="feira, trabalho, campo, água quente, distrito federal, sustentabilidade, agricultura" />
+        <meta name="keywords" content="feira, trabalho, campo, feira da torre, distrito federal, sustentabilidade, agricultura" />
         <meta name="author" content="Feira do Trabalho e do Campo DF" />
         <link rel="canonical" href="https://feiradotrabalhoedocampodf.com/" />
       </Helmet>
